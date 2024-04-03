7a76f53518776f2239f077cae261fd44309849a3
taiolamz <hassanadefarati@gmail.com>
2024-04-03 10:22:45 +0100

    adjusted the table responsiveness

--- src/reusables/tables/index.tsx
+++ src/reusables/tables/index.tsx
@@ -35,52 +35,54 @@ const TableContainer = ({
 
   return (
     <>
-      <div className="dashboard-table-wrap">
-        <TableHeader tableHeadData={tableHeadData} />
-        {children}
-      </div>
-      {/* pagination start */}
-      <div className="pagination-wrap">
-        {/* left wrap start */}
-        <div className="paginate-left-wrap">
-          <p>Showing</p>
-          <div className="select-wrap">
-            <Select
-              value={perPage}
-              options={pageOptions as any}
-              styles={reactSelectStyle({
-                bgColor: "rgba(33, 63, 125, 0.1)",
-                border: "none",
-                borderRadius: ".4rem",
-              })}
-              className="select-input"
-              onChange={(e: any) => setPerPage && setPerPage(e)}
+      {/* <div className="dashboard-table-container"> */}
+        <div className="dashboard-table-wrap">
+          <TableHeader tableHeadData={tableHeadData} />
+          {children}
+        </div>
+        {/* pagination start */}
+        <div className="pagination-wrap">
+          {/* left wrap start */}
+          <div className="paginate-left-wrap">
+            <p>Showing</p>
+            <div className="select-wrap">
+              <Select
+                value={perPage}
+                options={pageOptions as any}
+                styles={reactSelectStyle({
+                  bgColor: "rgba(33, 63, 125, 0.1)",
+                  border: "none",
+                  borderRadius: ".4rem",
+                })}
+                className="select-input"
+                onChange={(e: any) => setPerPage && setPerPage(e)}
+              />
+            </div>
+            <p>{`out of ${totalLengthArray}`}</p>
+          </div>
+          {/* left wrap end */}
+
+          {/* pagination wrap */}
+          <div className="paginate-wrap">
+            <ReactPaginate
+              breakLabel="..."
+              nextLabel=">"
+              onPageChange={handleClick && handleClick}
+              pageRangeDisplayed={3}
+              pageCount={pageCount ? pageCount : 0}
+              previousLabel="<"
+              marginPagesDisplayed={0}
+              containerClassName="pagination"
+              pageLinkClassName="page-num"
+              previousLinkClassName="page-num"
+              nextLinkClassName="page-num-next"
+              activeClassName="active"
+              nextClassName="next-btn"
+              previousClassName="prev-btn"
             />
           </div>
-          <p>{`out of ${totalLengthArray}`}</p>
         </div>
-        {/* left wrap end */}
-
-        {/* pagination wrap */}
-        <div className="paginate-wrap">
-          <ReactPaginate
-            breakLabel="..."
-            nextLabel=">"
-            onPageChange={handleClick && handleClick}
-            pageRangeDisplayed={3}
-            pageCount={pageCount ? pageCount : 0}
-            previousLabel="<"
-            marginPagesDisplayed={0}
-            containerClassName="pagination"
-            pageLinkClassName="page-num"
-            previousLinkClassName="page-num"
-            nextLinkClassName="page-num-next"
-            activeClassName="active"
-            nextClassName="next-btn"
-            previousClassName="prev-btn"
-          />
-        </div>
-      </div>
+      {/* </div> */}
       {/* pagination end */}
     </>
   );
